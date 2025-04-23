ee7beb59f128703722420e006a152ffe825d2c32
ptrbortolotti <ptrbortolotti@gmail.com>
2025-04-23 12:19:54 -0600

    dicts start to align

--- code/parametrize_runs.py
+++ code/parametrize_runs.py
@@ -4,25 +4,26 @@ import os
 
 run_dir = os.path.dirname(os.path.realpath(__file__))
 
-fsft_vt = {}
-fsft_vt["WT_X"] = [-780, 0, 780]
-fsft_vt["WT_Y"] = [0, 0, 0]
-fsft_vt["WT_Z"] = [0, 0, 0]
-fsft_vt["WT_FASTInFile"] = ["../turbines/IEA-3.4-130-RWT/IEA-3.4-130-RWT_T1.fst",
+fst_vt = {}
+fst_vt["FASTFarm"] = {}
+fst_vt["FASTFarm"]["WT_X"] = [-780, 0, 780]
+fst_vt["FASTFarm"]["WT_Y"] = [0, 0, 0]
+fst_vt["FASTFarm"]["WT_Z"] = [0, 0, 0]
+fst_vt["FASTFarm"]["WT_FASTInFile"] = ["../turbines/IEA-3.4-130-RWT/IEA-3.4-130-RWT_T1.fst",
                             "../turbines/IEA-3.4-130-RWT/IEA-3.4-130-RWT_T2.fst",
                             "../turbines/IEA-3.4-130-RWT/IEA-3.4-130-RWT_T3.fst"]
-fsft_vt["X0_High"] = [-820, -40, 740]
-fsft_vt["Y0_High"] = [-80, -80, -80]
-fsft_vt["Z0_High"] = [5, 5, 5]
-fsft_vt["dX_High"] = [5, 5, 5]
-fsft_vt["dY_High"] = [10, 10, 10]
-fsft_vt["dZ_High"] = [10, 10, 10]
+fst_vt["FASTFarm"]["X0_High"] = [-820, -40, 740]
+fst_vt["FASTFarm"]["Y0_High"] = [-80, -80, -80]
+fst_vt["FASTFarm"]["Z0_High"] = [5, 5, 5]
+fst_vt["FASTFarm"]["dX_High"] = [5, 5, 5]
+fst_vt["FASTFarm"]["dY_High"] = [10, 10, 10]
+fst_vt["FASTFarm"]["dZ_High"] = [10, 10, 10]
 
 output_path_fsft = "/Users/pbortolo/work/3_projects/30_HolisticSE/FarmCast/fastfarm/generated.FarmIEA3p4.fstf"
-generate_fsft(fsft_vt, output_path_fsft)
+generate_fsft(fst_vt, output_path_fsft)
 
-
-# Generate a TurbSim input file based on the provided fsft_vt dictionary.
+exit()
+# Generate a TurbSim input file based on the provided ts_vt dictionary.
 output_path_tsin = os.path.join(run_dir, "inflow_T1.in")
 ts_vt = {}
 write_turbsim_in(ts_vt, output_path_tsin)
--- code/write_fastfarm_fsft.py
+++ code/write_fastfarm_fsft.py
@@ -5,11 +5,11 @@ from validation import DefaultValidatingDraft7Validator
 run_dir = os.path.dirname(os.path.realpath(__file__))
 fastfarm_schema_path = os.path.join(run_dir, "fastfarm_schema.yaml")
 
-def generate_fsft(fsft_vt, output_path):
+def generate_fsft(fst_vt, output_path):
     """
-    Generate a FAST.Farm input file based on the provided fsft_vt dictionary.
+    Generate a FAST.Farm input file based on the provided fst_vt dictionary.
     Args:
-        fsft_vt (dict): Dictionary containing the FAST.Farm input parameters.
+        fst_vt (dict): Dictionary containing the FAST.Farm input parameters.
         output_path (str): Path to the output FAST.Farm input file.
     """
     # Check if the output path is valid
@@ -20,50 +20,62 @@ def generate_fsft(fsft_vt, output_path):
     with open(fastfarm_schema_path, 'r') as schema_file:
         fastfarm_schema = yaml.load(schema_file)
 
+    # Validate the input dictionary against the schema
     validator = DefaultValidatingDraft7Validator
-    validator(fastfarm_schema).validate(fsft_vt)
+    validator(fastfarm_schema).validate(fst_vt)
 
+    # Extract key order and descriptions from the schema
+    schema_properties = fastfarm_schema.get("properties", {}).get('FASTFarm', {}).get('properties', {})
+    key_order = list(schema_properties.keys())
+    descriptions = {key: schema_properties[key].get("description", "") for key in schema_properties}
+
+    fsft_vt = fst_vt["FASTFarm"]
 
     with open(output_path, 'w') as fsft_file:
         fsft_file.write("------- FAST.Farm for OpenFAST INPUT FILE -------------------------------------------------\n")
         fsft_file.write("Sample FAST.Farm input file\n")
         fsft_file.write("--- SIMULATION CONTROL ---\n")
         
-        for key, value in fsft_vt.items():
-            if isinstance(value, list):
-                value = ', '.join(map(str, value))
-            elif value is False:
-                value = "False"
-            elif value is True:
-                value = "True"
-            elif isinstance(value, str):
-                value = f"\"{value}\""
-            fsft_file.write(f"{str(value):<20} {key:<20}\n")
-            if key == "Mod_SharedMooring":
-                fsft_file.write("--- SUPER CONTROLLER --- [used only for UseSC=True]\n")
-            elif key == "SC_FileName":
-                fsft_file.write("--- SHARED MOORING SYSTEM --- [used only for Mod_SharedMoor>0]\n")
-            elif key == "WrMooringVis":
-                fsft_file.write("--- AMBIENT WIND: PRECURSOR IN VTK FORMAT --- [used only for Mod_AmbWind=1]\n")
-            elif key == "ChkWndFiles":
-                fsft_file.write("--- AMBIENT WIND: INFLOWWIND MODULE --- [used only for Mod_AmbWind=2 or 3]\n")
-            elif key == "InflowFile":
-                fsft_file.write("--- WIND TURBINES ---\n")
-                fsft_file.write("%d                  NumTurbines        - Number of wind turbines (-) [>=1]  [last 6 columns below used only for Mod_AmbWind=2 or 3]\n"%len(fsft_vt["WT_X"]))
-                fsft_file.write("WT_X    WT_Y   WT_Z    WT_FASTInFile\n")
-                fsft_file.write("(m)      (m)    (m)    (string)\n")
-                for i in range(len(fsft_vt["WT_X"])):
-                    fsft_file.write(f"{fsft_vt['WT_X'][i]:<8} {fsft_vt['WT_Y'][i]:<8} {fsft_vt['WT_Z'][i]:<8} \"{fsft_vt['WT_FASTInFile'][i]}\" {fsft_vt['X0_High'][i]:<8} {fsft_vt['Y0_High'][i]:<8} {fsft_vt['Z0_High'][i]:<8} {fsft_vt['dX_High'][i]:<8} {fsft_vt['dY_High'][i]:<8} {fsft_vt['dZ_High'][i]:<8}\n")
-                fsft_file.write("--- WAKE DYNAMICS ---\n")
-
-            elif key == "C_Meander":
-                fsft_file.write("--- CURLED-WAKE PARAMETERS [only used if Mod_Wake=2 or 3] ---\n")
-            elif key == "Mod_Projection":
-                fsft_file.write("--- WAKE-ADDED TURBULENCE ---\n")
-            elif key == "WAT_k_Grad":
-                fsft_file.write("--- VISUALIZATION ---\n")
-            elif key == "WrDisDT":
-                fsft_file.write("--- OUTPUT ---\n")
+        for key in key_order:
+            if key in fsft_vt:
+                value = fsft_vt[key]
+                if isinstance(value, list):
+                    value = ', '.join(map(str, value))
+                elif value is False:
+                    value = "False"
+                elif value is True:
+                    value = "True"
+                elif isinstance(value, str):
+                    value = f"\"{value}\""
+                description = descriptions.get(key, "")
+                fsft_file.write(f"{str(value):<20} {key:<20} {description}\n")
+                
+                # Add section headers based on specific keys
+                if key == "Mod_SharedMooring":
+                    fsft_file.write("--- SUPER CONTROLLER --- [used only for UseSC=True]\n")
+                elif key == "SC_FileName":
+                    fsft_file.write("--- SHARED MOORING SYSTEM --- [used only for Mod_SharedMoor>0]\n")
+                elif key == "WrMooringVis":
+                    fsft_file.write("--- AMBIENT WIND: PRECURSOR IN VTK FORMAT --- [used only for Mod_AmbWind=1]\n")
+                elif key == "ChkWndFiles":
+                    fsft_file.write("--- AMBIENT WIND: INFLOWWIND MODULE --- [used only for Mod_AmbWind=2 or 3]\n")
+                elif key == "InflowFile":
+                    fsft_file.write("--- WIND TURBINES ---\n")
+                    fsft_file.write("%d                  NumTurbines        - Number of wind turbines (-) [>=1]  [last 6 columns below used only for Mod_AmbWind=2 or 3]\n" % len(fsft_vt["WT_X"]))
+                    fsft_file.write("WT_X    WT_Y   WT_Z    WT_FASTInFile\n")
+                    fsft_file.write("(m)      (m)    (m)    (string)\n")
+                    for i in range(len(fsft_vt["WT_X"])):
+                        fsft_file.write(f"{fsft_vt['WT_X'][i]:<8} {fsft_vt['WT_Y'][i]:<8} {fsft_vt['WT_Z'][i]:<8} \"{fsft_vt['WT_FASTInFile'][i]}\" {fsft_vt['X0_High'][i]:<8} {fsft_vt['Y0_High'][i]:<8} {fsft_vt['Z0_High'][i]:<8} {fsft_vt['dX_High'][i]:<8} {fsft_vt['dY_High'][i]:<8} {fsft_vt['dZ_High'][i]:<8}\n")
+                    fsft_file.write("--- WAKE DYNAMICS ---\n")
+                elif key == "C_Meander":
+                    fsft_file.write("--- CURLED-WAKE PARAMETERS [only used if Mod_Wake=2 or 3] ---\n")
+                elif key == "Mod_Projection":
+                    fsft_file.write("--- WAKE-ADDED TURBULENCE ---\n")
+                elif key == "WAT_k_Grad":
+                    fsft_file.write("--- VISUALIZATION ---\n")
+                elif key == "WrDisDT":
+                    fsft_file.write("--- OUTPUT ---\n")
+        
         fsft_file.write('END of input file (the word "END" must appear in the first 3 columns of this last OutList line)\n')
         fsft_file.close()
 
